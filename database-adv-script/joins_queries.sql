b1b23cc59d4b5ea56234520130d1420596a48032
reuben-idan <nobeadonis2@gmail.com>
2025-05-17 08:42:27 +0000

    Write Complex Queries with Joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,27 @@
+SELECT 
+    bookings.id AS booking_id,
+    bookings.start_date,
+    bookings.end_date,
+    users.id AS user_id,
+    users.name AS user_name
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+
+SELECT 
+    properties.id AS property_id,
+    properties.title,
+    reviews.id AS review_id,
+    reviews.rating
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
