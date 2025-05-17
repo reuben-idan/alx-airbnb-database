16baa7363045ed06e064061af942583452783800
reuben-idan <nobeadonis2@gmail.com>
2025-05-17 10:13:07 +0000

    Refactor code snippet

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,20 @@
+-- Initial complex query to retrieve bookings, user, property, and payment details
+SELECT
+    b.id AS booking_id,
+    b.created_at AS booking_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    p.id AS property_id,
+    p.title AS property_title,
+    pay.id AS payment_id,
+    pay.amount AS payment_amount,
+    pay.status AS payment_status
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON pay.booking_id = b.id;
+
+
 -- Optimized version of the original query
 SELECT
     b.id AS booking_id,
